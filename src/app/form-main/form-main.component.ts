d5c49e7990cddb7756cf1cc71abaebcbfde6511b
Balu John Thomas <balujohnthomas@gmail.com>
2018-02-04 22:11:26 +0530

    First Commit

---
+++ src/app/form-main/form-main.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-main',
+  templateUrl: './form-main.component.html',
+  styleUrls: ['./form-main.component.css']
+})
+export class FormMainComponent implements OnInit {
+
+  constructor() { }
+
+  myClickFn(val){
+    console.log(val);
+  }
+
+  ngOnInit() {
+  }
+
+}
